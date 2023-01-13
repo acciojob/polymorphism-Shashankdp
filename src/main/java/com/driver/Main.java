f36cdc6b69e5d96ea12b67f7be5ebef9000742c9
shashank <shashankdpattar@gmail.com>
2023-01-13 17:25:46 +0530

    solution2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,10 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
         Product p=new Product();
-        p.product(x,y);
-        p.product(x,y,z);
-        p.product(x,y);
+
+        p.product(5,4);
+        p.product(5,4,2);
+        p.product(4.2,2.2);
     }
 
 }
